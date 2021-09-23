c76fba1fbc5adcf1890d478825480da0334cc9fb
Jak Fresko <maximkovolkoff@gmail.com>
2021-09-23 03:27:54 +0300

    commit all files

---
+++ Projwithclass/Comparator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projwithclass
+{
+    class Comparator
+    {
+       public int findMax (int first, int second)
+        {
+            int result;
+            if( first>second)
+                {
+                result = first;
+                }
+            else
+            {
+                result = second;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Projwithclass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projwithclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparator comparator = new Comparator();
+             
+
+            Console.WriteLine("Введи первое число");
+            string third = Console.ReadLine();
+            Console.WriteLine("Введи второе число");
+            string four = Console.ReadLine();
+           
+            int a = Convert.ToInt32(third);
+            int b = Convert.ToInt32(four);
+            int result = comparator.findMax(a, b);
+
+            Console.WriteLine("\n" + result + " является наибольшим числом");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
